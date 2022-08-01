05024da6c3a9355d48c1cdb1bc4f99109752a93c
Mohnish8899 <mohnishbaviskar99@gmail.com>
2022-08-01 16:49:33 +0530

    Hello World
    Files shifted to dotnetbatch folder

---
+++ text.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpTutorials 
+{
+    class pragram
+    {
+        static void Main()
+        {
+            string message = "Hello World!!";
+
+            System.Console.WriteLine(message);
+        }
+    }
+
+}
