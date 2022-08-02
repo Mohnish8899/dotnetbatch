8d8bc2c6233d2af0f65cc0118b447ed17d57cb01
Mohnish8899 <mohnishbaviskar99@gmail.com>
2022-08-02 15:49:43 +0530

    Problem no.1

---
+++ Mathematics1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math
+{
+    class operations
+    {
+        static void Main()
+        {
+            int num1 = 20;
+            int num2 = 30;
+            int result;
+
+            result = num1 + num2;
+            Console.WriteLine("Addition is {0}", result);
+
+            result = num1 - num2;
+            Console.WriteLine("Substraction is {0}", result);
+
+            result = num1 * num2;
+            Console.WriteLine("Multiplication is {0}", result);
+
+            result = num1 / num2;
+            Console.WriteLine("Division is {0}", result);
+
+
+        }
+    }
+}
