2597fba0c632ad6de5858e22e09fea44053109ba
Mohnish8899 <mohnishbaviskar99@gmail.com>
2022-08-22 16:39:47 +0530

    Functional component added

---
+++ React-Handson/myapp2/src/Cycle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cycle = ({brand,model}) => {
+    return (
+      <div>
+        <h1>
+          My cycle brand is {brand} and the model is {model}
+        </h1>
+      </div>
+    );
+}
+export default Cycle;
